2f354d2281e81232d87c12e5b1780a6c375fd7a6
PaolaNina <ninaflorespaola1@gmail.com>
2024-04-30 20:51:20 -0400

    endpoints 5 y 6

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = 3000; // puede cambiar
 
 //array 
 let librosBiblicos = [
-    {id: 1 , nombre: 'Genesis', autor: 'Moises'},
-    {id: 2 , nombre: 'Exodo', autor: 'Moises'},
-    {id: 3 , nombre: 'Levitico', autor: 'Moises'},
+    {id: 1 , nombre: 'Genesis', autor: 'Moises', anioPublicacion: 2020},
+    {id: 2 , nombre: 'Exodo', autor: 'Moises', anioPublicacion: 2024},
+    {id: 3 , nombre: 'Levitico', autor: 'Moises', anioPublicacion: 1990},
 ];
 //manejo de JSON
 app.use(express.json());
@@ -33,7 +33,34 @@ app.post('/agregar-libro', (req, res) => {
     librosBiblicos.push(nuevoLibro);
     res.status(201).json('este libro fue guardado exitosamente');
 })
-//
+// endpoint 4 Actualizar el libro
+app.put('/actualizar-libro/:id', (req, res) => {
+    const idCapturado = parseInt(req.params.id);
+    const indexLibroLocalizado = librosBiblicos.findIndex((libro) => libro.id === idCapturado);
+    if (indexLibroLocalizado !== -1 ){
+        librosBiblicos[indexLibroLocalizado] = req.body;
+        res.json(librosBiblicos[indexLibroLocalizado]);
+    } else {
+        res.status(404).json({mensaje : 'Libro no encontrado'});
+    }
+});
+// endpoint 5 Eliminar Libro
+app.delete('/eliminar-libro/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    lBiblico = librosBiblicos.filter( libro => libro.id !== id);
+    res.status(201).json({mensaje : 'se ha eliminado el libro'});
+    console.log(lBiblico);
+});
+//endpoint 6 
+app.get('/libros/publicacion/:anio', (req, res) => {
+    const year =  parseInt(req.params.anio);
+    const librosPublicados = librosBiblicos.filter( x => x.anioPublicacion === year);
+    if (librosPublicados.length > 0) {
+        res.json(librosPublicados);
+    } else {
+        res.status(404).json({mensaje : 'no se han encontrado libros publicados en ese anio'});
+    }
+});
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto http://localhost:" + PORT);
